8d475ed91783bbf0b46897e1543eb8ac4ac90487
4starChris <4star.christian@gmail.com>
2023-06-02 13:37:51 +1200

    Add project files.

---
+++ Project6_Minesweeper_Website/Logic/GameMaster.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Project6_Minesweeper_Website.Logic
+{
+    public class GameMaster
+    {
+        Square[,] Squarelist;
+
+        //Intermediate game is 16x16 with 40 mines
+        private int gameSizeWidth = 16; 
+        private int gameSizeHeight = 16;
+        private int numberOfMines = 40;
+
+
+        public GameMaster() { }
+
+        public void CreateGame()
+        {
+            Squarelist = new Square[gameSizeHeight, gameSizeWidth];
+
+            int row = 0;
+            int column = 0;
+            while (row < gameSizeHeight)
+            {
+                while (column < gameSizeWidth)
+                {
+                    Squarelist[column, row] = new Square();
+                    column++;
+                    
+                }
+                row++;
+                column = 0;
+            }
+
+            AssignMines();
+        }
+
+
+        private void AssignMines()
+        {
+            int i = 0;
+            Random rnd = new Random();
+            int row;
+            int column;
+            Square square;
+            while (i < numberOfMines)
+            {
+                row = rnd.Next(0, gameSizeHeight);
+                column = rnd.Next(0, gameSizeWidth);
+                square = Squarelist[column, row];
+
+                if (!square.clear) { continue; } //square already has a mine. Repeat loop.
+
+                square.AddMine();
+
+                //Add counter to neighbours
+                //Check left
+                if (column > 0) { Squarelist[column - 1, row].AddNearbyMine(); }
+
+                //Check right
+                if (column < 15) { Squarelist[column + 1, row].AddNearbyMine(); }
+
+                //Check up
+                if (row > 0) { Squarelist[column, row - 1].AddNearbyMine(); }
+
+                //Check down
+                if (row < 15) { Squarelist[column, row + 1].AddNearbyMine(); }
+
+                i++;
+            }
+        }
+    }
+}
---
+++ Project6_Minesweeper_Website/Logic/Square.cs
@@ -0,0 +1,35 @@
+﻿namespace Project6_Minesweeper_Website.Logic
+{
+    public class Square
+    {
+        public int nearbyMines { get; set; }
+        public bool clear { get; set; }
+
+        public Square() {
+            clear = true; 
+            nearbyMines = 0;
+        }
+
+        public void AddNearbyMine()
+        {
+            nearbyMines++;
+        }
+
+        public void AddMine()
+        {
+            clear = false;
+        }
+
+        public void CheckMine()
+        {
+            if (clear)
+            {
+                //display number of nearbyMines
+            }
+            else
+            {
+                //explode
+            }
+        }
+    }
+}
